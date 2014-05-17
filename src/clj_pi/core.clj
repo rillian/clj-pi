4cb26956b6e19ecec8c1914ea0f5c067d9423885
Ralph Giles <giles@thaumas.net>
2014-05-17 15:12:39 -0700

    Add monte-carlo circle? predicate, with tests.

--- src/clj_pi/core.clj
+++ src/clj_pi/core.clj
@@ -1,6 +1,11 @@
 (ns clj-pi.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; Monte-carlo sampling.
+; The idea here is to sample points in the plane, and count
+; how many fall inside the unit circle. This gives us an
+; estimate of the area, from which we can derive $\pi$.
+(defn circle?
+  "Test whether an x y pair is inside the using circle."
+  [x y]
+  ( >= 1.0 (+ (* x x) (* y y)))
+)
--- test/clj_pi/core_test.clj
+++ test/clj_pi/core_test.clj
@@ -2,7 +2,19 @@
   (:require [clojure.test :refer :all]
             [clj-pi.core :refer :all]))
 
-(deftest a-test
-  (testing "smoke test"
-    (foo "testing...")))
-
+(deftest circle-test
+  (testing "circle?"
+    ; Basic value checks.
+    (is (circle? 0 0))  ; origin is inside
+    (not (circle? 1 1)) ; outside
+    (not (circle? 1 0)) ; on the border
+    ; Check all four quadrants.
+    (is (circle? +0.5 +0.5))
+    (is (circle? +0.5 -0.5))
+    (is (circle? -0.5 +0.5))
+    (is (circle? -0.5 -0.5))
+    (not (circle? +0.8 +0.8))
+    (not (circle? +0.8 -0.8))
+    (not (circle? -0.8 +0.8))
+    (not (circle? -0.8 -0.8))
+))
